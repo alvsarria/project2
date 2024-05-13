6fa714966d4321a603a72c44d2d003ed1352619c
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-13 13:05:55 +0200

    footer v1.0

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import "../styles/components/Footer.css"
+import facebook from "../assets/images/facebook.png"
+import instagram from "../assets/images/instagram.png"
+import linkedin from "../assets/images/linkedin.png"
+import youtube from "../assets/images/youtube.png"
+import github from "../assets/images/github.png"
+
+const date = new Date();
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-major1">
+                <div className="footer-minor1">
+                    <p className="medievalfont">Wordsmith Retreat</p>
+                    <div className="footer-minor11">
+                        <p>privacy</p>
+                        <p>policy</p>
+                    </div>
+                </div>
+                <div className="footer-minor2">
+                    <div className="footer-minor21">
+                        <a href="https://www.facebook.com/" target="__blank">
+                            <img className="logoimage" src={facebook} alt="facebook icon" />
+                        </a>
+                        <a href="https://www.instagram.com/" target="__blank">
+                            <img className="logoimage" src={instagram} alt="instagram icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/" target="__blank">
+                            <img className="logoimage" src={linkedin} alt="linkedin icon" />
+                        </a>
+                        <a href="https://www.youtube.com/" target="__blank">
+                            <img className="logoimage" src={youtube} alt="youtube icon" />
+                        </a>
+                        <a href="https://www.github.com/" target="__blank">
+                            <img className="logoimage" src={github} alt="github icon" />
+                        </a>
+                    </div>
+                    <div className="footer-minor21">
+                        <p>Home</p>
+                        <p>Works</p>
+                        <p>About</p>
+                        <p>Blog</p>
+                        <p>Contact</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-major2">
+                <p>Copyright {date.getFullYear()}, All Rights Reserved.</p>
+            </div>
+        </div >
+    )
+};
+
+export default Footer;
