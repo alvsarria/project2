ae969ca6adbcbd8edffcbbec5a976fbca0e6cf33
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-14 18:51:30 +0200

    AllBooksPage v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import './App.css'
 import HomePage from './pages/HomePage.jsx';
+import AllBooksPage from './pages/AllBooksPage.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/books" element={<AllBooksPage />}></Route>
+      </Routes>
       <Footer />
     </>
   )
---
+++ src/pages/AllBooksPage.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import "../styles/pages/AllBooksPage.css"
+import supabase from "../utils/config";
+import bookmark_inactive from "../assets/images/bookmark.png"
+import bookmark_active from "../assets/images/bookmark_active.png"
+
+const AllBooksPage = () => {
+    const [arrayBooks, setArrayBooks] = useState([]);
+    const [bookmarkState, setBookmarkState] = useState(false);
+
+    const fetchData = async () => {
+        const { data, error } = await supabase
+            .from("books")
+            .select()
+            // .order("favorite", { ascending: false })
+            .order("id", { ascending: false })
+            .limit(50);
+        if (error) {
+            console.log(error);
+            return
+        } else {
+            setArrayBooks(data);
+            return
+        }
+    };
+
+    const displayDetailsModal = () => {
+        document.querySelector(".details-modal").style.visibility = "visible";
+    }
+
+    const hideDetailsModal = () => {
+        document.querySelector(".details-modal").style.visibility = "hidden";
+    }
+
+    const favoriteAddRemove = async (book) => {
+        if (book.favorite) {
+            const { error } = await supabase
+                .from('books')
+                .update({ favorite: false })
+                .eq('id', book.id)
+            if (error) {
+                console.log(error);
+                return
+            }
+        } else {
+            const { error } = await supabase
+                .from('books')
+                .update({ favorite: true })
+                .eq('id', book.id)
+            if (error) {
+                console.log(error);
+                return
+            }
+        }
+        setBookmarkState(!bookmarkState);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [bookmarkState]);
+
+    return (
+        <div className="allbookspage">
+            <div className="bookshelf">
+                {
+                    arrayBooks.map(book => {
+                        return (
+                            <div key={book.id} className="bookcard">
+                                <div className="book-container">
+                                    <div className="book">
+                                        <img src={book.favorite ? bookmark_active : bookmark_inactive} alt="bookmark icon" className="bookmark" onClick={() => favoriteAddRemove(book)} />
+                                        <img src={book.image} alt="book picture" className="bookpic" onClick={displayDetailsModal} />
+                                    </div>
+                                </div>
+                                <div className="shelf"></div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="details-modal" onClick={hideDetailsModal}>
+
+            </div>
+        </div>
+    )
+}
+
+export default AllBooksPage;
