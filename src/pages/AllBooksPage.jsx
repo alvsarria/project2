e6344de3b38aca65dd2109de8d51a1fb1816f315
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-05-15 16:00:00 +0200

    added search funcionality in all pages

--- src/pages/AllBooksPage.jsx
+++ src/pages/AllBooksPage.jsx
@@ -8,13 +8,34 @@ const AllBooksPage = () => {
     const [arrayBooks, setArrayBooks] = useState([]);
     const [bookDetail, setBookDetail] = useState({});
     const [showModal, setShowModal] = useState(false);
+    const [searchString, setSearchString] = useState("");
+
+    const handleSearchString = (e) => {
+        setSearchString(e.target.value);
+    };
+
+    const handleSarch = async () => {
+        const { data, error } = await supabase
+            .from("books")
+            .select()
+            .ilike("title", `%${searchString}%`);
+        if (error) {
+            console.log(error);
+        } else {
+            if (searchString === "") {
+                fetchData();
+            } else {
+                setArrayBooks(data);
+            }
+        }
+    };
 
     const fetchData = async () => {
         const { data, error } = await supabase
             .from("books")
             .select()
             .order("id", { ascending: false })
-            .limit(50);
+            .limit(200);
         if (error) {
             console.log(error);
             return
@@ -30,6 +51,7 @@ const AllBooksPage = () => {
 
     return (
         <div className="allbookspage" >
+            <input onChange={handleSearchString} type="text" name="searchbar" onKeyUp={(e) => e.key === "Enter" && handleSarch()} />
             <div className="bookshelf-allbooks">
                 {
                     arrayBooks.map(book => {
